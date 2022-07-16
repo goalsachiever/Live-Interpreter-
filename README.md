a2497d2d173dfa6bc97cdf90c74c16249f94c504
goalsachiever <goalsachiever8@gmail.com>
2022-07-16 23:57:56 +0530

    added

---
+++ README.md
@@ -0,0 +1,7 @@
+# MERN_TASK_1
+
+## Description:
+### Create a live HTML interpreter.
+
+## Overview:
+![image](https://user-images.githubusercontent.com/91736791/178230186-86c81c2a-9e8e-48cf-abac-c2e727d3d641.png)
---
+++ liveInterpreter.js
@@ -0,0 +1,8 @@
+
+let textArea = document.getElementById("editorBox");
+let previewArea = document.getElementById("previewBox");
+
+function onCodeChange() {
+    console.log(textArea.value);
+    previewArea.innerHTML = textArea.value;
+}
